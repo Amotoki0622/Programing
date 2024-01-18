142e1ecb410ddc041fbaaf7638c82c72ed1ae137
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2024-01-18 10:27:51 +0900

    RankingDispScene.h 0118

--- Game/DriveAndAvoid/Scene/RankingDispScene.h
+++ Game/DriveAndAvoid/Scene/RankingDispScene.h
@@ -1,1 +1,30 @@
 #pragma once
+
+
+#include "SceneBase.h"
+#include "../Object//RankingData.h"
+
+
+class RankingDispScene : public SceneBase
+{
+private:
+
+
+private:
+	int background_image;	//”wŒi‰æ‘œ
+	RankingData* ranking;	//ƒ‰ƒ“ƒLƒ“ƒO‰æ‘œ
+
+
+public:
+	RankingDispScene();
+	virtual ~RankingDispScene();
+
+
+	virtual void Initialize() override;
+	virtual  eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+	
+	
+	virtual void
+};
