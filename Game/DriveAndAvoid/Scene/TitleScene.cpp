e5d28946c6ed3bd1bee19b786d9240702468c591
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2024-01-25 09:28:09 +0900

    0125

---
+++ Game/DriveAndAvoid/Scene/TitleScene.cpp
@@ -0,0 +1,17 @@
+#include "TitleScene.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+TitleScene::TitleScene() : background_image(NULL), menu_image(NULL), cursor_image(NULL), menu_cursor(0)
+{
+
+}
+
+
+TitleScene::~TitleScene()
+{
+
+}
+
+
+//‰Šú‰»ˆ—
