c085025f86f17891200c28753826125cb3ff96fc
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2024-01-25 10:35:34 +0900

    InputControl.cpp 0125

--- Game/DriveAndAvoid/Utility/InputControl.h
+++ Game/DriveAndAvoid/Utility/InputControl.h
@@ -10,7 +10,7 @@ class InputControl
 private:
 	static bool now_button[16];	//現在フレーム入力値
 	static bool old_button[16];	//過去フレーム入力値
-	static float frigger[2];	//左右トリガー入力値
+	static float trigger[2];	//左右トリガー入力値
 	static Vector2D stick[2];	//左右スティック入力値
 
 
